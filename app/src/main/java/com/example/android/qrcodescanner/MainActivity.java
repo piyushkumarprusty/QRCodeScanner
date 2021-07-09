c4c3612c162a774a224e3b2e57bf86d70f33a76e
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-09 09:21:29 +0530

    design

--- app/src/main/java/com/example/android/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/example/android/qrcodescanner/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.budiyev.android.codescanner.CodeScanner;
 
+public class MainActivity extends AppCompatActivity {
+CodeScanner mCodeScanner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
