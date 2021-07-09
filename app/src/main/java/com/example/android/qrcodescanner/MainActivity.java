b104b9f81a2589671f3aaaa5224d579e64ae4bfe
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-09 09:23:29 +0530

    mcodescanner

--- app/src/main/java/com/example/android/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/example/android/qrcodescanner/MainActivity.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
 
 public class MainActivity extends AppCompatActivity {
-CodeScanner mCodeScanner;
+    CodeScanner mCodeScanner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
 
     }
 }
